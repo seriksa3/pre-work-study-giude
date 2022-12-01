b21f94d9056900ea902828433f496bf78df351c9
seriksa3 <kellyseriksa7@gmail.com>
2022-12-01 23:19:21 +0000

    Javascript introduction

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+//var shape = 'circle';
+var topic = "HTML";
+if (topic === "HTML") {
+    console.log("Let's study HTML");
+}
+else if (topic === 'CSS') {
+    console.log("Let's study CSS");
+}
+else if (topic === 'Git') {
+    console.log("Let's study Git");
+}
+else if (topic === 'Javascript') {
+    console.log("Let's study Javascript");
+}
+else {
+    console.log('Try Again');
+}
+
+var shapes = ["triangles","square","rectangle","circle"];
+console.log(shapes[1]);
+for (var x = 0; x < shapes.length; x++){
+    console.log(shapes[x]);
+}
